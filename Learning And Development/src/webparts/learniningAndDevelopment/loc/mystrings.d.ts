95b01a00f92cbe55ca383441d6cd0ee4af729cfa
JayPanara1998 <jaypanara@WWLx199407.onmicrosoft.com>
2026-08-31 14:33:14 +0530

    Learning And Development Webpart Setup

---
+++ Learning And Development/src/webparts/learniningAndDevelopment/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ILearniningAndDevelopmentWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'LearniningAndDevelopmentWebPartStrings' {
+  const strings: ILearniningAndDevelopmentWebPartStrings;
+  export = strings;
+}
